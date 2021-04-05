2b5654070c39c5dbeab2afb2c8f0bc06e3296bf9
Victor Misael <misael1805@gmail.com>
2021-04-05 16:45:40 -0300

    Add files via upload

--- TriggerTempoExecucao.sql
+++ TriggerTempoExecucao.sql
@@ -4,4 +4,4 @@ declare @playlistID int
 select @playlistID=playlist_cod_fk from inserted
 
 update Playlist set tempo_total=(select sum(tempoexecucao) from Faixa f where f.num_faixa in (select fp.faixa_num_faixa_fk from Faixa_Playlist fp
-where fp.playlist_cod_fk=@playlistID))
+where fp.playlist_cod_fk=@playlistID)) where Playlist.cod=@playlistID
